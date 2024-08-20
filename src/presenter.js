0fd1752bf8d528cbffff6bb615b8f57e5b6d2156
Santiago <aviles.barrientos.santiago.sa@gmail.com>
2024-08-19 22:53:22 -0400

    second hola

--- src/presenter.js
+++ src/presenter.js
@@ -1,14 +1,12 @@
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const Nombre = document.querySelector("#nombre");
+const form = document.querySelector("#hola-nombre");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const nombre = Nombre.value;
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>Hola, " + nombre + "!</p>";
 });
