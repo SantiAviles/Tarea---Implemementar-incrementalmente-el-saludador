4b0cf00a8382fee4a0b8f3ad4a2e57eccf479d35
Santiago <aviles.barrientos.santiago.sa@gmail.com>
2024-08-19 23:26:04 -0400

    fourth saludo

--- src/presenter.js
+++ src/presenter.js
@@ -1,19 +1,35 @@
 const nombre = document.querySelector("#nombre");
 const genero = document.querySelector("#genero");
+const edad = document.querySelector("#edad");
 const form = document.querySelector("#greeting-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  let saludo = "Hola ";  // Cambiado a 'let' para permitir la modificación
+  let saludo; 
   const nombreValue = nombre.value;
   const generoValue = genero.value;
+  const edadValue = Number(edad.value);
 
   if (generoValue.toLowerCase() === "masculino") {
-    saludo = "Hola Muchacho ";
+    if (edadValue <= 15){
+      saludo = "Hola niño "
+    } else if (edadValue > 15 && edadValue <= 30) {
+      saludo = "Hola joven "
+    } else if (edadValue > 30) {
+      saludo = "Hola señor "
+    }
   } else if (generoValue.toLowerCase() === "femenino") {
-    saludo = "Hola Muchacha ";
+    if (edadValue <= 15){
+      saludo = "Hola niña "
+    } else if (edadValue > 15 && edadValue <= 30) {
+      saludo = "Hola señorita "
+    } else if (edadValue > 30) {
+      saludo = "Hola señora "
+    }
+  } else {
+    saludo = "Hola";
   }
 
   div.innerHTML = "<p>" + saludo + nombreValue + "</p>";
